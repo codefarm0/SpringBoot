ee08b951645f3689cb284e456a24b833cf8f5c3d
Green Learner <whytechnology01@gmail.com>
2025-01-01 21:47:53 +0530

    liquibase

---
+++ liquibase-db-vcs/src/main/java/com/codefarm/liquibase/LiquibaseDbVcsApplication.java
@@ -0,0 +1,13 @@
+package com.codefarm.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDbVcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseDbVcsApplication.class, args);
+	}
+
+}
---
+++ liquibase-db-vcs/src/test/java/com/codefarm/liquibase/LiquibaseDbVcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefarm.liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseDbVcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
